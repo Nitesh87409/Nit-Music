6268c7cba3fae47c183000969537c1012b246f2d
Nitesh <niteshverma87409@gmail.com>
2026-07-02 14:25:34 +0530

    fix: import dart:ui for ImageFilter

--- lib/services/update_manager.dart
+++ lib/services/update_manager.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
-
+import 'dart:ui';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
